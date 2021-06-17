4aac253df7654a53ad639e7ecfeed3c5106dd23c
shivpx45 <85737556+shivpx45@users.noreply.github.com>
2021-06-17 15:06:36 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var btn_red;
+var btn_green;
+var r = 0;
+var g = 0;
+var b = 0;
+function setup() {
+createCanvas(1400, 800);  
+btn_red = createButton("red");
+btn_red.position(500,50);
+btn_red.mousePressed(red_bg);
+btn_green = createButton("green");
+btn_green.position(250,50);
+btn_green.mousePressed(green_bg);
+}
+function red_bg(){
+r = 255;
+g = 0;
+b = 0;
+}
+function green_bg(){
+r = 0;
+g = 255;
+b = 0;
+}
+function draw() {
+background(r,g,b);
+}
